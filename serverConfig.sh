bd3a09c76cfa273f016060fbe5dd786b47e67801
flint <drew.t.winston@gmail.com>
2021-08-30 16:51:57 -0400

    temp install var

--- serverConfig.sh
+++ serverConfig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Starting vanOS Config!"
 open 'smb://pi:1123H@192.168.11.230/share'
-echo -n "Enter Server ID"
+echo "Enter Server ID"
 read  ID
 #echo "Server ID $ID"
 
@@ -9,14 +9,15 @@ read  ID
 #defaults -currentHost write com.apple.screensaver idelTime 0
 
 #gets temp folder
-
+var temp_install = '~/Desktop/temp_install'
 if [[ $ID -gt 101 ]]
 then
-    cp -R /Volumes/share/All_City/-\ mBox\ files\ for\ install/Server\ Install ~/Desktop/temp_install
+    cp -R /Volumes/share/All_City/-\ mBox\ files\ for\ install/Server\ Install $temp_install
 else
-    cp -R /Volumes/share/All_City/-\ mBox\ files\ for\ install/Server\ 101\ Install ~/Desktop/temp_install
+    cp -R /Volumes/share/All_City/-\ mBox\ files\ for\ install/Server\ 101\ Install $temp_install
 fi
 
+sudo 
 
 
 
@@ -28,7 +29,7 @@ fi
 while true; do
     read -p "Done?" yn
     case $yn in
-        [Yy]* ) diskutil unmount /Volumes/share; rm -rf ~/Desktop/temp_install; break;;
+        [Yy]* ) diskutil unmount /Volumes/share; rm -rf $temp_install; break;;
         [Ny]*) echo 'Okay...'; exit;;
         *) echo "Please answer yes or no"
     esac
